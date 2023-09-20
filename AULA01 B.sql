03271ebf3a54333aa37d779ac1e353c85f733b44
MarcelloLM <106484381+MarcelloLM@users.noreply.github.com>
2023-09-20 10:54:59 -0300

    Update AULA01 B.sql

--- AULA01 B.sql
+++ AULA01 B.sql
@@ -12,3 +12,6 @@ AreaApartamento double NOT NULL,
 primary key (NumeroApartamento),
 constraint FKApart foreign key (CodEdificio) references Edificio(CodEdificios)
 );
+INSERT INTO Edificio(Endereco) values("Rua 3");
+INSERT INTO Apartamento(CodEdificio, NumeroApartamento, AreaApartamento) values(1, 69, 545);
+SELECT * FROM edificio; 
